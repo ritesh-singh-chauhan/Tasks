a4f5316ac05cf1d27aa844fae3ed252ca493cb0c
ritesh-singh-chauhan <ritesh@bytesview.com>
2023-08-11 11:16:11 +0530

    Update in pipeline

--- Task/pipelines.py
+++ Task/pipelines.py
@@ -3,7 +3,8 @@ from pymongo import MongoClient
 from ProcessCrawler import *
 from Task.items import TaskItem,FullDescription
 from CentralSql import CentralSql
-
+d={}
+mycon=CentralSql.Connection()
 class TaskPipeline:
     def process_item(self, item, spider):
         return item
@@ -12,7 +13,7 @@ class MongoDBPipeline:
     def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
-        self.mycon=CentralSql.Connection()
+        
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
@@ -29,18 +30,20 @@ class MongoDBPipeline:
             link=item['link']
             spider_fd=spider.name+"_fd"   #-------------------TO dynamically give the name of spider of the full description----------           
             try:
-                cursor=self.mycon.cursor()
-                cursor.execute(f"SELECT Domain.fdstatus FROM Source,Domain where Source.domain_id=Domain.id and Domain.name='{spider.name}'")
-                FdStatus=cursor.fetchall()
-                logging.info("Status of the FD",FdStatus)
 
-                if FdStatus[0][0]==1:
+                if spider.name not in d.keys():
+                    cursor=mycon.cursor()
+                    cursor.execute(f"SELECT Domain.fdstatus FROM Source,Domain where Source.domain_id=Domain.id and Domain.name='{spider.name}'")
+                    FdStatus=cursor.fetchall()
+                    d[spider.name]=FdStatus[0][0]
+                    logging.info("Status of the FD",FdStatus)
+
+                if d[spider.name]==1:
                     processObj=ProcessCrawler()
                     processObj.feed_fd(spider_fd,link)                    
             except Exception as error:
-                logging.error(f"Error Found in SQL Query :{error}")
-            finally:
-                cursor.close()
+                logging.error(f"Error Found in SQL Query pipeline:{error}")
+
 
             self.collection.insert_one(dict(item))
         if isinstance(item,FullDescription):
